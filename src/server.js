eea737c7c5a8060ea4847362db54343266af5ee8
gustavo <gmelo5805@gmail.com>
2023-12-06 16:06:12 -0300

    GET method

--- src/server.js
+++ src/server.js
@@ -3,4 +3,9 @@ const express = require("express")
 const app = express()
 
 const PORT = 3333
+
+app.get("/", (request,response) => {
+    response.send('Hello, world')
+})
+
 app.listen(PORT, () => console.log(`O server está rodando na porta: ${PORT}`))
