f5d8ef0b6b7687fb6ff5cdf5ab454c84d3a97980
gustavo <gmelo5805@gmail.com>
2023-12-07 18:14:35 -0300

    Error exception handling

--- src/server.js
+++ src/server.js
@@ -1,5 +1,8 @@
+require("express-async-error")
+
+const AppError = require('./utils/AppError')
 const express = require("express")
-const routes  = require("./routes")
+const routes = require("./routes")
 
 const app = express()
 app.use(express.json())
@@ -8,5 +11,20 @@ app.use(routes)
 
 const PORT = 3333
 
+app.use((error, request, response, next) => {
+  if (error instanceof AppError) {
+    return response.status(error.statusCode).json({
+      status: "error",
+      message: error.message
+    });
+  }
+
+  console.error(error);
+
+  return response.status(500).json({
+    status: error,
+    message: "Internal Server Error"
+  })
+})
 
 app.listen(PORT, () => console.log(`O server está rodando na porta: ${PORT}`))
