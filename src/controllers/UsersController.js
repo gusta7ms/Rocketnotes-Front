64771802ea650c35759b0f9afd8fec47627338b4
gustavo <gmelo5805@gmail.com>
2023-12-13 10:17:57 -0300

    Add Knex

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -58,17 +58,17 @@ class UsersController {
       user.password = await hash(password, 8)
     }
 
-    user.name = name;
-    user.email = email;
+    user.name = name ?? user.name;
+    user.email = email ?? user.email;
 
     database.run(`
       UPDATE users SET
       name = ?,
       email = ?,
       password = ?,
-      updated_at = ?
+      updated_at = DATETIME('now')
       WHERE id = ?`,
-      [user.name, user.email, user.password, new Date(), id]
+      [user.name, user.email, user.password, id]
       )
 
       return response.status(200).json()
