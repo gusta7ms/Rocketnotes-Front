01e216d319b61c29a8e5c78fb089a155d6c6b18b
gustavo <gmelo5805@gmail.com>
2023-12-18 20:23:24 -0300

    Logic of using CSS in JS

--- src/Pages/Details/index.jsx
+++ src/Pages/Details/index.jsx
@@ -1,10 +1,12 @@
-import "./styles.css"
+import { Conteiner } from "./styles"
 
 export  function Details (){
 
   return(
-    <>
+    <Conteiner>
       <h1>Hello Word!</h1>
-    </>
-  ) 
+
+      <h2>Gustavo Melo</h2>
+    </Conteiner>
+  )
 }
