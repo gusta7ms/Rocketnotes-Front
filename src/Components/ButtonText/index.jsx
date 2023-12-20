0b34ed89325577eff0b1cb2a1cc1aec4abebae15
gustavo <gmelo5805@gmail.com>
2023-12-20 16:52:00 -0300

    Finishing Details

---
+++ src/Components/ButtonText/index.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable react/prop-types */
+import { Container } from "./styles";
+
+export function ButtonText({ title, ...rest }) {
+  return (
+    <Container
+      {...rest}
+      type="button"
+    >
+      {title}
+    </Container>
+  );
+}
--- src/Pages/Details/index.jsx
+++ src/Pages/Details/index.jsx
@@ -2,11 +2,12 @@
 // ! PAGES DETAILS
 // ? PAGES DETAILS
 
-import { Container, Links } from "./styles";
+import { Container, Links, Content } from "./styles";
 
 import { Header } from "../../Components/Header";
-import { Button } from "../../Components/Button";
 import { Section } from "../../Components/Section";
+import { Button } from "../../Components/Button";
+import { ButtonText } from "../../Components/ButtonText";
 import { Tag } from "../../Components/Tag";
 
 export function Details() {
@@ -14,23 +15,43 @@ export function Details() {
     <Container>
       <Header />
 
-      <Section title="Links úteis">
-        <Links>
-          <li>
-            <a href="#">https://rocketseat.com.br</a>
-          </li>
-          <li>
-            <a href="#">https://rocketseat.com.br</a>
-          </li>
-        </Links>
-      </Section>
+      <main>
+        <Content>
+          <ButtonText title="Excluir Nota"></ButtonText>
 
-      <Section title="Marcadores">
-        <Tag title="express" />
-        <Tag title="node" />
-      </Section>
+          <h1>Introdução ao React</h1>
 
-      <Button title="Voltar" />
+          <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry standard dummy text ever
+            since the 1500s, when an unknown printer took a galley of type and
+            scrambled it to make a type specimen book. It has survived not only
+            five centuries, but also the leap into electronic typesetting,
+            remaining essentially unchanged. It was popularized in the 1960s
+            with the release of Letraste sheets containing Lorem Ipsum passages,
+            and more recently with desktop publishing software like Ladus
+            PageMaker including versions of Lorem Ipsum.
+          </p>
+
+          <Section title="Links úteis">
+            <Links>
+              <li>
+                <a href="#">https://rocketseat.com.br</a>
+              </li>
+              <li>
+                <a href="#">https://rocketseat.com.br</a>
+              </li>
+            </Links>
+          </Section>
+
+          <Section title="Marcadores">
+            <Tag title="express" />
+            <Tag title="node" />
+          </Section>
+
+          <Button title="Voltar" />
+        </Content>
+      </main>
     </Container>
   );
 }
