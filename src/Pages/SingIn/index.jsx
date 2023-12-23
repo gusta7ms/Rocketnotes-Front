b18d6e0b5b335d1d9b2510dc732f0dc410b88af6
gustavo <gmelo5805@gmail.com>
2023-12-23 19:19:09 -0300

    created SingIn page

---
+++ src/Pages/SingIn/index.jsx
@@ -0,0 +1,29 @@
+// TODO PAGES SingIN
+
+import { FiMail, FiLock } from "react-icons/fi";
+
+import { Container, Form } from "./styles";
+
+import { Input } from "../../components/Input";
+import { Button } from "../../components/Button";
+
+export function SingIn() {
+  return (
+    <Container>
+      <Form>
+        <h1>Rocket Notes</h1>
+        <p>Aplicação para salvar e gerenciar sues links uteis</p>
+
+        <h2>Faça seu login</h2>
+
+        <Input placeholder="email" type="text" icon={FiMail} />
+
+        <Input placeholder="email" type="password" icon={FiLock} />
+
+        <Button title="Entrar" />
+
+        <a href="#">Criar Conta</a>
+      </Form>
+    </Container>
+  );
+}
