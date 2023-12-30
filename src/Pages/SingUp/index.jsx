cd61ec37a47b38c96eda3f52ec4869851cd4f8ff
gustavo <gmelo5805@gmail.com>
2023-12-30 17:30:30 -0300

    Logic implementation for the Registration page with the database

--- src/Pages/SingUp/index.jsx
+++ src/Pages/SingUp/index.jsx
@@ -1,17 +1,44 @@
-// TODO PAGES SingIN
+import { api } from "../../services/api";
 
-import { FiUser, FiMail, FiLock} from "react-icons/fi";
+import { useState } from "react";
+import { FiUser, FiMail, FiLock } from "react-icons/fi";
 
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
-import { Container, Form, Background} from "./styles";
+import { Container, Form, Background } from "./styles";
 import { Input } from "../../components/Input";
 import { Button } from "../../components/Button";
 
 export function SingUp() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const navigate = useNavigate();
+
+  function handleSingUp() {
+    if (!name || !email || !password) {
+      return alert("por favor preencha todos os campos.");
+    }
+
+    api
+      .post("/users", { name, email, password })
+      .then(() => {
+        alert("Usuário cadastrado com sucesso");
+        navigate("/")
+      })
+      .catch((error) => {
+        if (error.response) {
+          alert(error.response.data.message);
+        } else {
+          ("Não foi possível cadastrar o usuário, tente novamente.");
+        }
+      });
+  }
+
   return (
     <Container>
-      <Background/>
+      <Background />
 
       <Form>
         <h1>Rocket Notes</h1>
@@ -19,17 +46,31 @@ export function SingUp() {
 
         <h2>Crie sua conta</h2>
 
-        <Input placeholder="Usuário" type="text" icon={FiUser} />
+        <Input
+          placeholder="Usuário"
+          type="text"
+          icon={FiUser}
+          onChange={(e) => setName(e.target.value)}
+        />
 
-        <Input placeholder="email" type="text" icon={FiMail} />
+        <Input
+          placeholder="email"
+          type="text"
+          icon={FiMail}
+          onChange={(e) => setEmail(e.target.value)}
+        />
 
-        <Input placeholder="password" type="password" icon={FiLock} />
+        <Input
+          placeholder="password"
+          type="password"
+          icon={FiLock}
+          onChange={(e) => setPassword(e.target.value)}
+        />
 
-        <Button title="Cadastrar seu novo usuário" />
+        <Button title="Cadastrar seu novo usuário" onClick={handleSingUp} />
 
         <Link to="/">Voltar para login</Link>
       </Form>
-
     </Container>
-  )
+  );
 }
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,12 +1,13 @@
 import { BrowserRouter } from "react-router-dom";
 
-import { AppRoutes } from "./app.routes";
-// import { AuthRoutes } from "./auth.routes";
+// import { AppRoutes } from "./app.routes";
+import { AuthRoutes } from "./auth.routes";
 
 export function Routes() {
   return (
     <BrowserRouter>
-      <AppRoutes />
+      {/* <AppRoutes /> */}
+      <AuthRoutes />
     </BrowserRouter>
   );
 }
