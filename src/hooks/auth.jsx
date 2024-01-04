0a5884e033966bd6724d63c5019002ae69e685e5
gustavo <gmelo5805@gmail.com>
2024-01-04 18:45:03 -0300

    Using Local Storage

--- src/hooks/auth.jsx
+++ src/hooks/auth.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable react-refresh/only-export-components */
 /* eslint-disable react/prop-types */
 import { useState } from "react";
-import { createContext, useContext } from "react";
+import { createContext, useContext, useEffect } from "react";
 
 import { api } from "../services/api";
 
@@ -15,6 +15,9 @@ function AuthProvider({ children }) {
       const response = await api.post("/sessions", { email, password });
       const { user, token } = response.data;
 
+      localStorage.setItem("rocketNotes:user", JSON.stringify(user));
+      localStorage.setItem("rocketNotes:token", token);
+
       api.defaults.headers.authorization = `Bearer ${token}`;
 
       setData({ user, token });
@@ -26,6 +29,20 @@ function AuthProvider({ children }) {
       }
     }
   }
+
+  useEffect(() => {
+      const user = localStorage.getItem("rocketNotes:user");
+      const token = localStorage.getItem("rocketNotes:token" );
+
+      if(token && user) {
+        api.defaults.headers.authorization = `Bearer ${token}`;
+
+        setData({
+          token,
+          user: JSON.parse(user)
+        })
+      }
+  }, [])
 
   return (
     <AuthContext.Provider value={{ singIn, user: data.user }}>
